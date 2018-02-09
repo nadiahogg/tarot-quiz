d20421c3b19b1c27d4840461bf04db22ed598909
Nadia Hogg <hogg.nadia@gmail.com>
2018-02-09 11:45:24 -0500

    updated correct answer on click

--- js/script.js
+++ js/script.js
@@ -74,11 +74,161 @@ tarotQuiz.quiz = {
         "Which tarot card is often associated with a feeling of being trapped?",
       options: ["The Chariot", "The Tower", "Temperance", "The Devil"],
       correct: 3
+    },
+    {
+      text: "Which card is often a sign of being overwhelmed?",
+      options: ["The World", "The Moon", "The Star", "The Chariot"],
+      correct: 0
+    },
+    {
+      text:
+        "Which card is sometimes considered to be first and foremost about self-control?",
+      options: ["The Hierophant", "The Emperor", "The Empress", "The Fool"],
+      correct: 1
+    },
+    {
+      text: "Which card often appears when things are far from being clear?",
+      options: ["The Star", "The Moon", "The Sun", "Justice"],
+      correct: 1
+    },
+    {
+      text:
+        "Which card doesn't guarantee success but says go ahead and make a wish anyway - there is always hope?",
+      options: [
+        "The Magician",
+        "The Wheel of Fortune",
+        "The Star",
+        "The World"
+      ],
+      correct: 2
+    },
+    {
+      text: "Which card is an indication that change is inevitable?",
+      options: ["Death", "Temperance", "The Magician", "Strength"],
+      correct: 0
+    },
+    {
+      text:
+        "Which card sometimes indicates that what goes around comes around?",
+      options: ["The Wheel of Fortune", "The World", "Death", "Justice"],
+      correct: 3
+    },
+    {
+      text: "Which card is referred to as the Pisces of the tarot deck?",
+      options: ["The High Priestess", "The Empress", "The Star", "The Moon"],
+      correct: 0
+    },
+    {
+      text:
+        "Which card often appears when it seems as if the whole world has turned upside down?",
+      options: ["The Chariot", "The Hanged Man", "The Tower", "The Devil"],
+      correct: 0
+    },
+    {
+      text:
+        "Which card generally means that you should be on the lookout for things that are going to be important and meaningful?",
+      options: ["Death", "Strength", "The Hierophant", "Justice"],
+      correct: 1
+    },
+    {
+      text:
+        "Which tarot card can sometimes indicate the possibility of pregnancy?",
+      options: ["The High Priestess", "The Empress", "The Moon", "The Star"],
+      correct: 1
     }
   ]
 };
 
-
+tarotQuiz.cards = [
+  {
+    cardName: "The Fool",
+    fileName: "00_fool"
+  },
+  {
+    cardName: "The Magician",
+    fileName: "01_magician"
+  },
+  {
+    cardName: "The High Priestess",
+    fileName: "02_highpriestess"
+  },
+  {
+    cardName: "The Empress",
+    fileName: "03_empress"
+  },
+  {
+    cardName: "The Emperor",
+    fileName: "04_emperor"
+  },
+  {
+    cardName: "The Hierophant",
+    fileName: "05_hierophant"
+  },
+  {
+    cardName: "The Lovers",
+    fileName: "06_lovers"
+  },
+  {
+    cardName: "The Chariot",
+    fileName: "07_chariot"
+  },
+  {
+    cardName: "Strength",
+    fileName: "08_strength"
+  },
+  {
+    cardName: "The Hermit",
+    fileName: "09_hermit"
+  },
+  {
+    cardName: "The Wheel of Fortune",
+    fileName: "10_wheelfortune"
+  },
+  {
+    cardName: "Justice",
+    fileName: "11_justice"
+  },
+  {
+    cardName: "The Hanged Man",
+    fileName: "12_hangedman"
+  },
+  {
+    cardName: "Death",
+    fileName: "13_death"
+  },
+  {
+    cardName: "Temperance",
+    fileName: "14_temperance"
+  },
+  {
+    cardName: "The Devil",
+    fileName: "15_devil"
+  },
+  {
+    cardName: "The Tower",
+    fileName: "16_tower"
+  },
+  {
+    cardName: "The Star",
+    fileName: "17_star"
+  },
+  {
+    cardName: "The Moon",
+    fileName: "18_moon"
+  },
+  {
+    cardName: "The Sun",
+    fileName: "19_sun"
+  },
+  {
+    cardName: "Judgment",
+    fileName: "20_judgement"
+  },
+  {
+    cardName: "The World",
+    fileName: "21_world"
+  }
+];
 
 tarotQuiz.newQuestion = function() {
     //check number of questions that the user has completed.
@@ -124,7 +274,7 @@ tarotQuiz.newQuestion = function() {
       } 
       else {
         console.log("Da jam done son!!");
-        $(".question-view").addClass("hidden");
+        $(".question-view").addClass("hide");
         $(".quiz-end-page").removeClass("hidden");
         if (tarotQuiz.quiz.score < 5) {
                  $(".score-display").text("Nice try, this result simply means you're at the beginning of your journey...kinda like The Fool" + "You scored " + tarotQuiz.quiz.score + " out of " + 10);
@@ -134,38 +284,72 @@ tarotQuiz.newQuestion = function() {
                 $(".score-display").text("Wow, you're a true Star!" + "You scored " + tarotQuiz.quiz.score + " out of " + 10);
               }
       }
+    $(".options").addClass("active");  
     });
       
     
     $(".options").on('click','li',function() {
+      if ($(".options").hasClass("active")) {
         console.log('linked clicked')
         let guess = $(this).attr('id');
+        $(".options").removeClass("active");
         $(this).addClass("selected");
         $(".next-question").removeClass("hidden");
         tarotQuiz.checkAnswer(guess);
+      }
+    });
+
+    
+  
+
+    $(".options").on("mouseover", "li", function() {
+      if ($(".options").hasClass("active")) {
+      let optionName = (this.innerText);
+      for (let i = 0; i < tarotQuiz.cards.length; i = i + 1) {
+        if (optionName == tarotQuiz.cards[i].cardName){
+          $(".card-image").attr("src", "./assets/" + tarotQuiz.cards[i].fileName + ".jpg");
+        }
+      }
+    }
+
+    });
+
+    $(".options").on("mouseleave", "li", function() {
+      console.log("left area")
+      $(".card-image").removeAttr("src");
     });
 
     $(".restart").click(function(event) {
       tarotQuiz.quiz.current = 0;
       tarotQuiz.quiz.score = 0;
-      $(".quiz-end-page").addClass("hide");
+      $(".quiz-end-page").addClass("hidden");
       $(".score").addClass("hide");
       $(".feedback").html("");
       $(".remaining-questions").html("");
       $(".quiz-start-page").show();
     });
 
+
+
+
+
     tarotQuiz.checkAnswer = function(guess) {
       const currentAnswer = tarotQuiz.quiz.randomQuestion;
 	if (guess == tarotQuiz.quiz.currentAnswer.correct) {
     console.log("correct!");
-    $(".feedback").append(`<p>You got it right!</p>`); 
+    $(".feedback").append(`<p>You got it right! The correct answer is</p>`); 
     tarotQuiz.quiz.score++;
   } else {
     console.log("try again");
     $(".feedback").append(`<p>Sorry, that's incorrect.</p>`);
+    $("#" + tarotQuiz.quiz.currentAnswer.correct).addClass("correct-answer");
   }
-}
+ 
+};
+
+// tarotQuiz.showImage = function(correctCard) {
+//   const correctCard = tarotQuiz.quiz.currentAnswer.;
+// }
 
 
 
@@ -197,7 +381,7 @@ $(document).ready(function() {
 	$(".start").click(function(event) {
         console.log("Clicked Start Button");
         $(".quiz-start-page").hide();
-		$(".question-view").removeClass("hidden");
+		$(".question-view").removeClass("hide");
         event.preventDefault();
         tarotQuiz.newQuestion();
     })
